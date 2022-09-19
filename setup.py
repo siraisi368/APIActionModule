ec72f0459408e3492135298deaf4a24f0ed5574c
Masato Siraisi <zhaitengkong5@gmail.com>
2022-09-19 21:41:36 +0900

    Upload File

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="APIactModule",
+    version="1.4.0",
+    author="MasatoSiraisi",
+    url="https://github.com/siraisi368/APIActionModule",
+    download_url="https://github.com/siraisi368/APIActionModule",
+    license="Apache License2.0",
+    install_requires=["json","requests","discord","ntplib","datetime","time","xmltodict","difflib"],
+    python_requires=">=3.6"
+    )
